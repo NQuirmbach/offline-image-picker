8f38fefbd7b49935be15a84444afd0a1d8d7a182
Nils Quirmbach <nils.quirmach@outlook.com>
2025-02-10 18:42:32 +0100

    bump

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,8 +3,35 @@
     <h2>PWA Info</h2>
     <pre>{{ JSON.stringify($pwa, null, 2) }}</pre>
   </div>
+
+  <div class="flex items-centers space-x-4 my-4">
+    <button @click="handleInstall">Install PWA</button>
+    <button @click="handleUpdateSW">Update SW</button>
+    <button @click="handleGetSWRegistration">Get SW registration</button>
+  </div>
+  <pre v-if="message">{{ message }}</pre>
 </template>
 
 <script setup lang="ts">
 const { $pwa } = useNuxtApp();
+
+const message = ref<string | null>(null);
+
+async function handleInstall() {
+  const result = await $pwa?.install();
+  message.value = result ? JSON.stringify(result) : "empty";
+}
+async function handleUpdateSW() {
+  await $pwa?.updateServiceWorker(true);
+}
+async function handleGetSWRegistration() {
+  const result = $pwa?.getSWRegistration();
+  message.value = result ? JSON.stringify(result) : "empty";
+}
 </script>
+
+<style lang="postcss" scoped>
+button {
+  @apply p-2 bg-gray-600 rounded-sm text-white;
+}
+</style>
